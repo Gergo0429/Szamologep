c452855b1de041b3e3f0ca09735d657b57169159
Gergo0429 <zsigmondgergo@gmail.com>
2024-05-07 14:36:39 +0300

    Kesz

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Linq.Expressions;
+
+namespace Szamologep.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string? Elso { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Masodik { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public char Operator { get; set; }
+
+        public double? Eredmeny { get; set; }
+
+        public string? HibaUzenet { get; set; }
+
+        public void OnPost()
+        {
+			if (Elso == null || Masodik == null)
+			{
+				HibaUzenet = "Kérem 2 darab számot adjon meg!";
+			}
+			else if (!double.TryParse(Elso, out double elso) || !double.TryParse(Masodik, out double masodik))
+			{
+				HibaUzenet = "Kérem számokat adjon meg!";
+			}
+			else
+			{
+				switch (Operator)
+                {
+                    case '+':
+                        Eredmeny = elso + masodik;
+                        break;
+                    case '-':
+                        Eredmeny = elso - masodik;
+                        break;
+                    case '*':
+                        Eredmeny = elso * masodik;
+                        break;
+                    case '/':
+                        Eredmeny = elso / masodik;
+                        break;
+                    default:
+                        Eredmeny = null;
+                        break;
+                }
+			}
+		}
+    }
+}
